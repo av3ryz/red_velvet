18ca16cbc861d97cdb57a4ae650275bce62035b3
av3ryz <averyzng@gmail.com>
2024-12-04 00:09:26 -0500

    R3.3

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,5 +3,11 @@ class AlbumsController < ApplicationController
   end
 
   def show
+    album_id = params[:id]
+    album_sql = sanitize_sql(["SELECT * FROM albums WHERE id = ?", album_id])
+    @album = db.execute(album_sql).first
+    
+    songs_sql = sanitize_sql(["SELECT * FROM songs WHERE album_id = ?", album_id])
+    @songs = db.execute(songs_sql)
   end
 end
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,12 @@
 class ArtistsController < ApplicationController
   def index
     sql = "SELECT * FROM artists"
-    @artists = ActiveRecord::Base.connection.execute(sql)
+    @artists = db.execute(sql)
   end
 
   def show
     artist_id = params[:id]
-    sql = ActiveRecord::Base.send(:sanitize_sql_array, ["SELECT * FROM artists WHERE id = ?", artist_id])
-    @artist = ActiveRecord::Base.connection.execute(sql).first
+    sql = sanitize_sql(["SELECT * FROM artists WHERE id = ?", artist_id])
+    @artist = db.execute(sql).first
   end
 end
