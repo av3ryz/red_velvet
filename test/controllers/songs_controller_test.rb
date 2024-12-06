71d9fd2e1b23ef78c542117132055887fa5a9150
Avery Zeng <avery.zeng@shopify.com>
2024-12-05 22:39:15 -0500

    songs test

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -1,13 +1,14 @@
-require "test_helper"
+require 'test_helper'
 
 class SongsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get songs_index_url
+    get songs_path
     assert_response :success
   end
 
   test "should get show" do
-    get songs_show_url
+    song = songs(:one) 
+    get song_path(song)
     assert_response :success
   end
 end
